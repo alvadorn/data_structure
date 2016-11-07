6e124370f77bc6855f0080ada202838407a5a19c
Igor Sant'ana <igorba2005@gmail.com>
2016-11-07 00:20:37 -0200

    Fixing errors appointed in Travis

--- src/queue.c
+++ src/queue.c
@@ -21,7 +21,7 @@ void Queue__enqueue(Queue *queue, void *value) {
   if (queue != NULL) {
     struct __queue_node *tmp = (struct __queue_node *) malloc (sizeof(struct __queue_node));
     tmp->next = NULL;
-    if (queue->size = 0) {
+    if (queue->size == 0) {
       queue->queue = tmp;
     } else {
       queue->last->next = tmp;
